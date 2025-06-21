92a0bc3a4722d450b39c2a1ca42335f60b0c4862
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-21 19:10:02 -0300

    Add logic to sort blocks

--- src/index.js
+++ src/index.js
@@ -18,7 +18,28 @@ function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-function startRaceEngine(character1, character2) {}
+function getRandomBlock() {
+  const randomValue = Math.random();
+
+  switch (true) {
+    case randomValue < 0.33:
+      return "RETA";
+    case randomValue < 0.66:
+      return "CURVA";
+    default:
+      return "CONFRONTO";
+  }
+}
+
+function startRaceEngine(character1, character2) {
+  for (let round = 1; round <= 5; round++) {
+    console.log(`Rodada ${round}`);
+
+    // Sort block
+    const block = getRandomBlock();
+    console.log(`Bloco: ${block}`);
+  }
+}
 
 function main() {
   console.log(`Corrida entre ${player1.nome} e ${player2.nome} começando...\n`);
